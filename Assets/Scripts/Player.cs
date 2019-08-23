16555fa3be3ad9c37514b4c964c6fe2b32a83429
LouisG97 <j_k_h@live.com.mx>
2019-08-22 20:25:17 -0700

    Core assets
    Core assets added

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+  [SerializeField,Range(0.1f,10f)]
+  float moveSpeed;
+
+  void Update(){
+      Vector2 axis = new Vector2(Input.GetAxis("Horizontal"),
+      Input.GetAxis("Vertical"));
+
+      transform.Translate(axis * moveSpeed * Time.deltaTime);
+  }
+
+}
