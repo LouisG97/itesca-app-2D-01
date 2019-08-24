bfd1bc71d116a164e6912be881b4e0e51515fd0e
LouisG97 <j_k_h@live.com.mx>
2019-08-23 23:19:15 -0700

    Movimiento Personaje
    Animacion de los 4 movimientos de personaje

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,11 +7,32 @@ public class Player : MonoBehaviour
   [SerializeField,Range(0.1f,10f)]
   float moveSpeed;
 
+  Animator animator;
+  SpriteRenderer spriteRenderer;
+
+  bool moving;
+
+  void Awake(){
+    animator = GetComponent<Animator>();
+    spriteRenderer = GetComponent<SpriteRenderer>();
+  }
+
   void Update(){
       Vector2 axis = new Vector2(Input.GetAxis("Horizontal"),
       Input.GetAxis("Vertical"));
 
       transform.Translate(axis * moveSpeed * Time.deltaTime);
+
+      moving = axis != Vector2.zero;
+
+      if(moving){
+        animator.SetFloat("move-X",axis.x);
+        animator.SetFloat("move-Y",axis.y);
+      }
+
+      animator.SetBool("moving",moving);
+
+      spriteRenderer.flipX = axis.x < 0 ? true : axis.x > 0 ? false : spriteRenderer.flipX;
   }
 
 }
